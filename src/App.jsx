4e3afca5fc88a43daed4738e6261fb93e18608cf
AJPascoe <pascoe312@gmail.com>
2021-08-05 10:24:36 +0100

    Template Hompage

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,40 @@ import './App.css';
 
 const App = () => {
   return (
-  <div>Hello Adam </div>
+  <div className="home-page">
+    <h1>Cat-Fisher</h1>
+    <h2>Picking the best partner for the job</h2>
+    <div>
+      <div className="home-image"></div>
+      <div className="pruducts">
+        <div>box1
+          <button className="Add"></button>
+        </div>
+        <div>box2
+        <button className="Add"></button>
+        </div>
+        <div>box3
+        <button className="Add"></button>
+        </div>
+        <div>box4
+        <button className="Add"></button>
+        </div>
+        <div>box5
+        <button className="Add"></button>
+        </div>
+        <div>box6
+        <button className="Add"></button>
+        </div>
+      </div>
+    </div>
+  </div>
+
   )
 }
 
 
+
 export default App;
+
+
+
