cfcc3689a8af2858c83ed5414daa2378f297b9dc
PuiYanLeung <trewina@gmail.com>
2021-08-07 23:36:10 +0100

    Fetch API data

--- src/App.jsx
+++ src/App.jsx
@@ -1,127 +1,98 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import './App.css'
 import Products from './components/Products'
 import Cart from './components/Cart'
 
 const PAGE_PRODUCTS = 'products'
 const PAGE_CART = 'cart'
+const API_URL = 'https://api.thecatapi.com/v1/images/search?limit=67'
+//const API_URL = 'https://api.thecatapi.com/v1/breeds'
+// const apiKey = `1ef81c01-f0b1-4971-9655-2017bd4c6bfc`
 
 function App() {
 
-  useEffect (() => {
-    fetchItems();
-  }, []);
+  // shopping cart
+  const [cart, setCart] = useState([])
+  const [page, setPage] = useState(PAGE_PRODUCTS)
+  const [products, setProducts] = useState(null)
+  const [isInputProductCost, setInputProductCost] = useState(false)
+  //fetch items
+  const [loading, setLoading] = useState(true)
+  const [error, setError] = useState(null)
 
-  const [items, setItems] = useState([]);
-  
-  // shopping cart variable
-  const [cart, setCart] = useState([]);
-  const [page, setPage] = useState(PAGE_PRODUCTS);
-
+  useEffect(() => {
+    fetchItems()
+  }, [])
 
   const fetchItems = async () => {
-    try {
-      const data = await fetch(
-        'https://api.thecatapi.com/v1/images/search?limit=6'
-      );
-      const items = await data.json();
-      console.log(items);
-      setItems(items);
-    } catch (error) {
-      console.log(error);
-    }
-  };
-  
-  const addToCart = (product) =>{
-    console.log('we are in addToCart')
-    setCart([...cart, product])
-    console.log(`${cart}`)
+    fetch(API_URL)
+    .then(response => {
+      if (response.ok) {
+        return response.json()
+      }
+      throw response
+    })
+    .then(data => {
+      setProducts(data)
+    })
+    .catch(error => {
+      console.error("Error fetching data: ", error)
+      setError(error)
+    })
+    .finally(() => {
+      setLoading(false)
+    })
   }
 
-  const removeFromCart = (productToRemove) =>{
+  const inputProductCost = (products) => {
+    const cost = Math.ceil(((Math.random() * (10 - 2)) + 2)) * 50
+    products.map((product) => (
+      product.cost = cost
+    ))
+    setInputProductCost(true)
+  }
+
+  const addToCart = (product) => {
+    setCart([...cart, product])
+  }
+
+  const removeFromCart = (productToRemove) => {
     setCart(
-      cart.filter((product)=>product!==productToRemove)
+      cart.filter((product) => product !== productToRemove)
     )
   }
 
-  const navigateTo = (nextPage)=>{
+  const navigateTo = (nextPage) => {
     setPage(nextPage)
   }
 
+  if (loading) return "Loading"
+  if (error) return "Error"
   return (
-
     <div className='wrapper'>
       <div className='bubbles'>
-      <div className='bubble1'></div>
-      <div className='bubble2'></div>
-      <div className='bubble3'></div>
-      <div className='bubble4'></div>
-      <div className='bubble5'></div>
-      <div className='bubble6'></div>
-    </div>
-    <h1 className='title'>CAT-FISHER</h1>
-    <h2 className='subtitle'>Picking the right partner for the job</h2>
-    <div className='home-img'></div>
-      
-
-    <div className='products'></div>
-    {/* Or use nav bar */}
-    <header>
-      <button onClick={()=> navigateTo(PAGE_CART)}>Go to Cart({cart.length})</button>
-      <button onClick={()=> navigateTo(PAGE_PRODUCTS)}>View Products</button>
-    </header>      
-    {page === PAGE_PRODUCTS && <Products addToCart={addToCart}/>}
-    {page === PAGE_CART && <Cart cart={cart} removeFromCart={removeFromCart}/>}
-    
-    {/* Fetch data from API */}
-      {/* 
-    <div className='proudcts'>
-      {items.map(data => (
-          <img src={data.url} alt='cat' />
-      ))}
+        <div className='bubble1'></div>
+        <div className='bubble2'></div>
+        <div className='bubble3'></div>
+        <div className='bubble4'></div>
+        <div className='bubble5'></div>
+        <div className='bubble6'></div>
       </div>
-    </div> */}           
+      <h1 className='title'>CAT-FISHER</h1>
+      <h2 className='subtitle'>Picking the right partner for the job</h2>
+      <div className='home-img'></div>
 
-  </div> 
-  
+      <div className='products'></div>
+      {/* Button can be replaced by nav bar */}
+      <header>
+        <button onClick={() => navigateTo(PAGE_CART)}>Go to Cart({cart.length})</button>
+        <button onClick={() => navigateTo(PAGE_PRODUCTS)}>View Products</button>
+      </header>
+      {!isInputProductCost && inputProductCost(products)}
+      {page === PAGE_PRODUCTS && <Products products={products} addToCart={addToCart} />}
+      {page === PAGE_CART && <Cart cart={cart} removeFromCart={removeFromCart} />}
+    </div>
   );
 };
 
 export default App;
-
-// Planning
-//  App = () => {
-//   return (
-//   <div className="home-page">
-//     <h1>Cat-Fisher</h1>
-//     <h2>Picking the best partner for the job</h2>
-//     <div>
-//       <div className="home-image"></div>
-//       <div className="pruducts">
-//         <div>box1
-//           <button className="Add"></button>
-//         </div>
-//         <div>box2
-//         <button className="Add"></button>
-//         </div>
-//         <div>box3
-//         <button className="Add"></button>
-//         </div>
-//         <div>box4
-//         <button className="Add"></button>
-//         </div>
-//         <div>box5
-//         <button className="Add"></button>
-//         </div>
-//         <div>box6
-//         <button className="Add"></button>
-//         </div>
-//       </div>
-//     </div>
-//   </div>
-
-//   )
-// }
-// const apiKey = `1ef81c01-f0b1-4971-9655-2017bd4c6bfc`;
-
-// const url = `https://api.thecatapi.com/v1/images/search?limit=6`;
--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,11 +1,11 @@
-import React, {useState} from 'react'
+import React from 'react'
 
 export default function Cart({cart, removeFromCart}){
     return(  <>          
         {cart.map((product, idx) =>(
             <div className='product' key={idx}>
-              <img src={product.image} alt={product.id} height='200px' width='200px'/>
-              <h3>{product.cost}</h3>          
+              <img src={product.url} alt={product.id} height='200px' width='200px'/>
+              <h3>£{product.cost}</h3>          
               <button onClick={()=>removeFromCart(product)}>
                     Remove
                   </button>
